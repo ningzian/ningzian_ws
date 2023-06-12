6e3adbb117e282dad1a67091b14e82fcf201183d
Zian Ning <846940235@qq.com>
2023-06-12 13:38:50 +0800

    test

--- src/Onboard-SDK-ROS/src/dji_osdk_ros/iusl_img_manage.cpp
+++ src/Onboard-SDK-ROS/src/dji_osdk_ros/iusl_img_manage.cpp
@@ -10,6 +10,7 @@ std::string weight_file = "/home/dji/ningzian_ws/src/Onboard-SDK-ROS/script/dark
 int gim_max_speed = 8;
 float gim_control_k = 0.015;
 
+
 // global param
 Detector detector(cfg_file, weight_file, 0);     // darknet detector
 ros::Time now_time;   // 当前时间
